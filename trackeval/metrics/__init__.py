882f4423416e206dafd0ded9b3c0096dbec5a407
amirassov <amirassov@gmail.com>
2021-06-10 13:36:44 +0200

    Add TrackIdentity to __init__

--- trackeval/metrics/__init__.py
+++ trackeval/metrics/__init__.py
@@ -1,6 +1,6 @@
 from .hota import HOTA
 from .clear import CLEAR
-from .identity import Identity
+from .identity import Identity, TrackIdentity
 from .count import Count
 from .j_and_f import JAndF
 from .track_map import TrackMAP
